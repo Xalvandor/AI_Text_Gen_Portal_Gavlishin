12691e4073cfaa802943236f99f5e87e5d0051e3
Xalvandor <nikolaygavlishin@gmail.com>
2026-04-26 12:06:20 -0400

    added html and css to display output of samples

---
+++ app.py
@@ -0,0 +1,28 @@
+import random
+import sys
+
+from flask import Flask, jsonify, send_from_directory, render_template
+import subprocess
+import torch
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate', methods=['GET'])
+def generate_text():
+    result = subprocess.run(
+        [
+            sys.executable, 'sample.py', f'--seed={random.randint(0,2000)}',
+        ],
+        capture_output=True, text=True
+    )
+    print(result)
+    print(result.stdout)
+    return jsonify({"output": result.stdout})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
--- sample.py
+++ sample.py
@@ -10,9 +10,9 @@ from model import GPTConfig, GPT
 
 # -----------------------------------------------------------------------------
 init_from = 'resume' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
-out_dir = '../out'  # ignored if init_from is not 'resume'
+out_dir = 'out'  # ignored if init_from is not 'resume'
 start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
-num_samples = 10 # number of samples to draw
+num_samples = 1 # number of samples to draw
 max_new_tokens = 500 # number of tokens generated in each sample
 temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
 top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
@@ -59,7 +59,7 @@ if init_from == 'resume' and 'config' in checkpoint and 'dataset' in checkpoint[
     meta_path = os.path.join('data', checkpoint['config']['dataset'], 'meta.pkl')
     load_meta = os.path.exists(meta_path)
 if load_meta:
-    print(f"Loading meta from {meta_path}...")
+    # print(f"Loading meta from {meta_path}...")
     with open(meta_path, 'rb') as f:
         meta = pickle.load(f)
     # TODO want to make this more general to arbitrary encoder/decoder schemes
@@ -86,4 +86,4 @@ with torch.no_grad():
         for k in range(num_samples):
             y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
             print(decode(y[0].tolist()))
-            print('---------------')
+            #print('---------------')
